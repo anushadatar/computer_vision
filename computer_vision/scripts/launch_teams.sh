ab02c9ea0a7e7aaed495865eb564691fa5de5dba
Siddharth Garimella <gsidsid@gmail.com>
2020-11-02 17:36:27 -0500

    kill gzserver if hangs

--- computer_vision/scripts/launch_teams.sh
+++ computer_vision/scripts/launch_teams.sh
@@ -4,4 +4,4 @@ python2 team_one_actor.py 2 & \
 python2 team_one_actor.py 3 & \
 python2 team_two_actor.py 1 & \
 python2 team_two_actor.py 2 & \
-python2 team_two_actor.py 3
+python2 team_two_actor.py 3 
